c8bbdc3423cf9f59c402ee159822bf24f022428e
yuralala8 <yc1435@nyu.edu>
2017-07-24 14:24:29 -0400

    rails url helpers lab

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,12 +3,5 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
-  #
-  # def active
-  #   if self.active
-  #     "This student is currently inactive."
-  #   else
-  #     "This student is currently active."
-  #   end
-  # end
+
 end
