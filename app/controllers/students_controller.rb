ebfa24158f285eb8955f7618259ae4e38c95bbd9
yuralala8 <yc1435@nyu.edu>
2017-07-24 14:19:19 -0400

    rails url helpers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @students = Student.all
+    set_student
+  end
+
+  def update
+    set_student
+    if @student.active == true
+      @student.active = false
+    else
+      @student.active = true
+    end
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,14 @@
 class Student < ActiveRecord::Base
+
   def to_s
     self.first_name + " " + self.last_name
   end
+  #
+  # def active
+  #   if self.active
+  #     "This student is currently inactive."
+  #   else
+  #     "This student is currently active."
+  #   end
+  # end
 end
